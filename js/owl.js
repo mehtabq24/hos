dcd880ac49b2d6f5e938adce18b527a94e41ad92
mdarbaz98 <zainshaikh2468@gmail.com>
2023-06-21 22:32:15 +0530

    first commit

---
+++ js/owl.js
@@ -0,0 +1,24 @@
+$('.banner-carousel').owlCarousel({
+    loop:true,
+    dots:true,
+    responsiveClass:true,
+    items: 1,
+})
+
+
+$('.p__carousel').owlCarousel({
+    loop:true,
+    margin:20,
+    dots:true,
+    responsiveClass:true,
+    items: 5,
+})
+
+
+$('.product').owlCarousel({
+    loop:true,
+    margin:20,
+    dots:true,
+    responsiveClass:true,
+    items: 1,
+})
