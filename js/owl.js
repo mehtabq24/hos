2a6215c7901f621d75d6c160fa8f3f939882981f
mdarbaz98 <zainshaikh2468@gmail.com>
2023-06-21 23:57:38 +0530

    home page mobile view done

--- js/owl.js
+++ js/owl.js
@@ -11,7 +11,18 @@ $('.p__carousel').owlCarousel({
     margin:20,
     dots:true,
     responsiveClass:true,
-    items: 5,
+    // items: 5,
+        responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 5
+        },
+    }
 })
 
 
